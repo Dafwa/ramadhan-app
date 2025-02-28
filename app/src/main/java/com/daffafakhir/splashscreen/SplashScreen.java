a5ee2a16fd60df0d42e86b0824e43a93c043b132
Dafwa <daffafahir@gmail.com>
2025-02-28 15:38:22 +0800

    Menambahkan force light mode agar tampilan aman

--- app/src/main/java/com/daffafakhir/splashscreen/SplashScreen.java
+++ app/src/main/java/com/daffafakhir/splashscreen/SplashScreen.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +15,8 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // force light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
@@ -27,6 +30,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
             }
         },2000);
 
